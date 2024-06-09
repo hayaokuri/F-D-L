f4af7d86fe8095390280a4568f378e3bce54a693
Hayaokuri <58360045+hayaokuri@users.noreply.github.com>
2024-06-09 16:52:25 +0900

    日本語化

--- webhook.py
+++ webhook.py
@@ -31,98 +31,83 @@ def topLogin(data: list) -> None:
         "content": None,
         "embeds": [
             {
-                "title": "FGO登录系统 - " + main.fate_region,
-                "description": f"登录成功。列出角色信息.\n\n{messageBonus}",
+                "title": "ログインに成功しました",
+                "description": f"連続ログイン、f"{login.login_days}" 日目です。\n\n{messageBonus}",
                 "color": 563455,
                 "fields": [
                     {
-                        "name": "御主名",
+                        "name": "マスター名",
                         "value": f"{name1}",
                         "inline": True
                     },
                     {
-                        "name": "朋友ID",
-                        "value": f"{fpids1}",
-                        "inline": True
-                    },
-                    {
-                        "name": "等级",
+                        "name": "ランク",
                         "value": f"{rewards.level}",
                         "inline": True
                     },
                     {
-                        "name": "呼符", 
+                        "name": "呼符数", 
                         "value": f"{rewards.ticket}",
                         "inline": True
                     },                    
                     {
-                        "name": "圣晶石",
+                        "name": "聖晶石",
                         "value": f"{rewards.stone}",
                         "inline": True
                     },
                     {
-                        "name": "圣晶片",
-                        "value": f"{rewards.sqf01}",
-                        "inline": True
-                    },
-                    {
-                        "name": "金苹果",
+                        "name": "金リンゴ",
                         "value": f"{rewards.goldenfruit}",
                         "inline": True
                     },
                     {
-                        "name": "银苹果",
+                        "name": "銀リンゴ",
                         "value": f"{rewards.silverfruit}",
                         "inline": True
                     },
                     {
-                        "name": "铜苹果",
+                        "name": "銅リンゴ",
                         "value": f"{rewards.bronzefruit}",
                         "inline": True
                     },
                     {
-                        "name": "蓝苹果",
+                        "name": "青リンゴ",
                         "value": f"{rewards.bluebronzefruit}",
                         "inline": True
                     },
                     {
-                        "name": "蓝苹果树苗",
+                        "name": "青銅の苗木",
                         "value": f"{rewards.bluebronzesapling}",
                         "inline": True
                     },
                     {
-                        "name": "连续登录天数",
-                        "value": f"{login.login_days}",
-                        "inline": True
-                    },
-                    {
-                        "name": "累计登录天数",
-                        "value": f"{login.total_days}",
-                        "inline": True
-                    },
-                    {
-                        "name": "白方块",
+                        "name": "ストームポッド",
                         "value": f"{rewards.pureprism}",
                         "inline": True
                     },
                     {
-                        "name": "友情点",
+                        "name": "連続ログイン日数",
+                        "value": f"{login.login_days}",
+                        "inline": True
+                    },
+                    {
+                        "name": "通算ログイン日数",
+                        "value": f"{login.total_days}",
+                        "inline": True
+                    },
+                    {
+                        "name": "フレンドポイント",
                         "value": f"{login.total_fp}",
                         "inline": True
                     },
                     {
-                        "name": "今天 获得的友情点",
+                        "name": "今回獲得したフレンドポイント",
                         "value": f"+{login.add_fp}",
                         "inline": True
                     },
                     {
-                        "name": "当前AP",
+                        "name": "現在のAP",
                         "value": f"{login.remaining_ap}",
-                        "inline": True
-                    },
-                    {
-                        "name": "圣杯",
-                        "value": f"{rewards.holygrail}",
                         "inline": True
                     },
                     
@@ -149,13 +134,13 @@ def shop(item: str, quantity: str) -> None:
         "content": None,
         "embeds": [
             {
-                "title": "FGO自动购物系统 - " + main.fate_region,
-                "description": f"购买成功.",
+                "title": "青りんごを購入しました",
+                "description": f"アイテム交換だね。いいよ。",
                 "color": 5814783,
                 "fields": [
                     {
-                        "name": f"商店",
-                        "value": f"消费 {40 * quantity}Ap 购买 {quantity}x {item}",
+                        "name": f"ダ・ヴィンチ工房",
+                        "value": f"消費 {40 * quantity}Ap 購入 {quantity}x {item}",
                         "inline": False
                     }
                 ],
